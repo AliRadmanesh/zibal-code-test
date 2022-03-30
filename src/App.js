5c93632d8a1ed673a022cffe0af40b794d4e919b
AliRadmanesh <ali.radmanesh1995@gmail.com>
2022-03-30 10:43:26 +0430

    Add createComponent & json test files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import data from "./assets/data.json";
+import data from "./assets/data2.json";
+import createComponent from "./utils/createComponent";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {createComponent({
+        key: data.length,
+        component: data,
+      })}
     </div>
   );
-}
+};
 
 export default App;
